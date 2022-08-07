c7de1dbe3163ecd1aff4e93e817e2f3b0fcc1754
nuuco <nuuco.coding@gmail.com>
2022-08-08 02:20:13 +0900

    feat: 페이지 라우팅 완료

--- react-vendingmachine/src/App.jsx
+++ react-vendingmachine/src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 // SPA 구성 라이브러리
-// import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';
 import Management from './pages/Management';
 import Charge from './pages/Charge';
 import Product from './pages/Product';
@@ -18,12 +18,14 @@ function App() {
   /* react-router-dom으로 SPA를 먼저 구성해주세요. */
   return (
     <React.Fragment>
-        <button id='management-button'>상품 관리</button>
-        <button id='charge-button'>잔돈 충전</button>
-        <button id='product-button'>상품 구매</button>
-        <Management products={products} setProducts={setProducts}/>
-        <Charge coins={coins} setCoins={setCoins}/>
-        <Product products={products} setProducts={setProducts} coins={coins} setCoins={setCoins}/>
+        <Link to='/'><button id='management-button'>상품 관리</button></Link>
+        <Link to='/charge'><button id='charge-button'>잔돈 충전</button></Link>
+        <Link to='/product'><button id='product-button'>상품 구매</button></Link>
+      <Routes>
+        <Route path='/' element={<Management products={products} setProducts={setProducts}/>}/>
+        <Route path='/charge' element={<Charge coins={coins} setCoins={setCoins}/>}/>
+        <Route path='/product' element={<Product products={products} setProducts={setProducts} coins={coins} setCoins={setCoins}/>}/>
+      </Routes>
     </React.Fragment>
   );
 }
