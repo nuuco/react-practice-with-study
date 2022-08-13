f233c8c222efcc7aae6dd60e8fdbee54ceba036a
nuuco <nuuco.coding@gmail.com>
2022-08-13 10:40:42 +0900

    feat: 라우팅 구현 완료

--- react-vendingmachine/src/App.jsx
+++ react-vendingmachine/src/App.jsx
@@ -9,13 +9,7 @@ function App() {
   // products의 요소 데이터 형식 { product: "솔의눈", price: "1000", count: "1" }
   const [products, setProducts] = useState([]);
   // 잔돈의 데이터 형식입니다.
-  const [coins, setCoins] = useState({ 500: 0, 100: 0, 50: 0, 10: 0 });
-  /* API 호출 상수 */
-  // const Random = global.MissionUtils.Random;
-  // const randomNumber = Random.pickNumberInRange(1, 10);
-  /* API 예시 입니다. 확인하시고 지우시면 됩니다. */
-  // console.log(randomNumber);
-  /* react-router-dom으로 SPA를 먼저 구성해주세요. */
+  const [coins, setCoins] = useState({ "500": 0, "100": 0, "50": 0, "10": 0 });
   return (
     <React.Fragment>
         <Link to='/'><button id='management-button'>상품 관리</button></Link>
